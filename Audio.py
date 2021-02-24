43b1b1e8ae47d435b836a01208e9096df394c5b9
Kelkar-Nikita <nkkelkar100@gmail.com>
2021-02-24 14:11:54 +0530

    select notes as per our choice

--- Audio.py
+++ Audio.py
@@ -1,5 +1,4 @@
 import pyttsx3
 speaker = pyttsx3.init()
-#audio msg
 speaker.say("Hlo sweetheart")
 speaker.runAndWait()
--- atm.py
+++ atm.py
@@ -3,6 +3,7 @@ notes_denom=(2000,500,200,100,50,20,10,5,2,1)
 # unchangeble tuple
 print("select denomination:")
 counter=0
+
 while cash > 0:
     print("do you want %d Y/N...?"%notes_denom[counter],end=" ")
     resp = input()
